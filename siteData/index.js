98164a5c46af0d3b27d780445a475ca302a89577
CDFMej <funesmejiac@gmail.com>
2025-06-28 22:37:25 -0600

    first commit

---
+++ siteData/index.js
@@ -0,0 +1,7 @@
+import indexData from './pages/data';
+function context(page){
+    context = {...indexData}
+    return context;
+}
+
+export default context;
---
+++ siteData/pages/data.js
@@ -0,0 +1,95 @@
+ const datosLiga = {
+  equipos: [
+    {
+      nombre: "Olimpia",
+      partidosJugados: 18,
+      partidosGanados: 13,
+      partidosEmpatados: 3,
+      golesFavor: 40,
+      golesContra: 15,
+      posicion: 1
+    },
+    {
+      nombre: "Motagua",
+      partidosJugados: 18,
+      partidosGanados: 11,
+      partidosEmpatados: 5,
+      golesFavor: 34,
+      golesContra: 18,
+      posicion: 2
+    },
+    {
+      nombre: "Marathón",
+      partidosJugados: 18,
+      partidosGanados: 10,
+      partidosEmpatados: 4,
+      golesFavor: 29,
+      golesContra: 20,
+      posicion: 3
+    },
+    {
+      nombre: "Real España",
+      partidosJugados: 18,
+      partidosGanados: 9,
+      partidosEmpatados: 5,
+      golesFavor: 27,
+      golesContra: 23,
+      posicion: 4
+    },
+    {
+      nombre: "Victoria",
+      partidosJugados: 18,
+      partidosGanados: 7,
+      partidosEmpatados: 6,
+      golesFavor: 22,
+      golesContra: 24,
+      posicion: 5
+    },
+    {
+      nombre: "Vida",
+      partidosJugados: 18,
+      partidosGanados: 6,
+      partidosEmpatados: 6,
+      golesFavor: 20,
+      golesContra: 26,
+      posicion: 6
+    },
+    {
+      nombre: "Lobos UPNFM",
+      partidosJugados: 18,
+      partidosGanados: 5,
+      partidosEmpatados: 5,
+      golesFavor: 19,
+      golesContra: 28,
+      posicion: 7
+    },
+    {
+      nombre: "Real Sociedad",
+      partidosJugados: 18,
+      partidosGanados: 4,
+      partidosEmpatados: 6,
+      golesFavor: 18,
+      golesContra: 30,
+      posicion: 8
+    },
+    {
+      nombre: "Olancho FC",
+      partidosJugados: 18,
+      partidosGanados: 3,
+      partidosEmpatados: 5,
+      golesFavor: 15,
+      golesContra: 31,
+      posicion: 9
+    },
+    {
+      nombre: "Génesis",
+      partidosJugados: 18,
+      partidosGanados: 2,
+      partidosEmpatados: 4,
+      golesFavor: 13,
+      golesContra: 32,
+      posicion: 10
+    }
+  ]
+};
+export default datosLiga;
